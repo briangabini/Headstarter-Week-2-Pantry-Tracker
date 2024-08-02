5ee2b4439d28d773f3412e000913536d41d4b3c0
Brian Gabini <91449132+briangabini@users.noreply.github.com>
2024-08-02 15:49:39 +0800

    fix: fix errors

--- firebase.js
+++ firebase.js
@@ -4,6 +4,11 @@ import { getFirestore } from "firebase/firestore";
 import dotenv from 'dotenv';
 dotenv.config();
 
+// Ensure all required environment variables are present
+if (!process.env.FIREBASE_API_KEY || !process.env.FIREBASE_AUTH_DOMAIN || !process.env.FIREBASE_PROJECT_ID || !process.env.FIREBASE_STORAGE_BUCKET || !process.env.FIREBASE_MESSAGING_SENDER_ID || !process.env.FIREBASE_APP_ID || !process.env.FIREBASE_MEASUREMENT_ID) {
+    throw new Error("Missing Firebase configuration values");
+}
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
